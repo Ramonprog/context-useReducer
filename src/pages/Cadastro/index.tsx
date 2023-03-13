968bde182a782887efc1ad443a88dceb08613d0e
ramonprog <programador.ramon@gmail.com>
2023-03-12 21:33:22 -0300

    rotas criadas

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cadastro = () => {
+  return (
+    <div>
+      <h1>cadastro</h1>
+    </div>
+  );
+};
+
+export default Cadastro;
---
+++ src/pages/MostrarDados/index.tsx
@@ -0,0 +1,9 @@
+const MostrarDados = () => {
+  return (
+    <div>
+      <h1>mostrar dados</h1>
+    </div>
+  );
+};
+
+export default MostrarDados;
